c5b173da9c565d3efc8f29991dc031ba6d730788
UrielData911 <uriel.dominguez@data911.eu>
2022-08-25 09:08:52 +0200

    añadido setGastoEditar al Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Mensaje from './Mensaje'
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar}) => {
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar, setGastoEditar}) => {
     const [mensaje, setMensaje] = useState('')
     const [nombre, setNombre] = useState('')
     const [cantidad, setCantidad] = useState('')
